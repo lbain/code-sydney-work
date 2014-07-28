b3f99b4d846f2c8daf49c3e7692f43c06835bcc2
Lucy Bain <lucybain@gmail.com>
2014-07-28 18:37:20 +1000

    Make the game an object too - Thanks Brad!

--- week-3/game.js
+++ week-3/game.js
@@ -1,102 +1,108 @@
-var board;
-var size = 18;
-var timer = 0;
-var food;
-var snake;
-var direction = 'r';
+function Game() {
+  var direction = 'r';
+  var size = 18;
+  var board;
+  var timer;
+  var food;
+  var snake;
+  var me = this;
 
-function changePosition(direction) {
-  var deltas = {'r' : [1,0],
-                'l' : [-1,0],
-                'u' : [0,-1],
-                'd' : [0,1]};
-  return deltas[direction];
-};
+  this.init = function() {
+    board = new Board('#board tbody', size);
+    snake = new Snake(size);
+    food = new Food(size);
+    board.display();
+    this.display();
+    timer = setInterval(function() {
+      me.moveAndDisplay();
+    }, 500);
+    this.bindKeys();
+  };
 
-function oppositeDirection(newDirection) {
-  var opposites = {
-    'r': 'l',
-    'l': 'r',
-    'u': 'd',
-    'd': 'u'
-  }
-  return opposites[direction] === newDirection
-}
+  this.bindKeys = function() {
+    $(document).keydown(function(e){
+      var arrowKeys = { 37 : 'l',
+                        38 : 'u',
+                        39 : 'r',
+                        40 : 'd'};
+      newDirection = arrowKeys[e.keyCode];
+      if (me.oppositeDirection(newDirection) && snake.hasBody()) {
+        me.endGame();
+      } else {
+        direction = newDirection;
+      }
+    });
+  };
 
+  this.display = function() {
+    snake.display();
+    food.display();
+  };
 
-function hitEdge() {
-  return (snake.head()[0] > size || // hit right
-          snake.head()[0] < 0    || // hit left
-          snake.head()[1] > size || // hit bottom
-          snake.head()[1] < 0)      // hit top
-}
+  this.moveAndDisplay = function() {
+    snake.move(this.delta());
+    this.hitFood();
+    this.checkEndGame();
+    this.display();
+  };
 
-function checkEndGame() {
-  if (snake.hitSelf() || hitEdge()) {
-    endGame();
-  }
-}
+  this.changePosition = function(direction) {
+    var deltas = {'r' : [1,0],
+                  'l' : [-1,0],
+                  'u' : [0,-1],
+                  'd' : [0,1]};
+    return deltas[direction];
+  };
 
-function endGame() {
-  clearInterval(timer);
-  displayFinal();
-}
-
-function displayFinal() {
-  console.log('You lose :(');
-  board.clear();
-}
-
-function delta() {
-  return changePosition(direction);
-}
-
-function hitFood() {
-  if(snake.hitCell(food.getLocation())) {
-    snake.lengthen(delta());
-    food.generate(size);
-    return true;
-  }
-  return false;
-}
-
-function display() {
-  snake.display();
-  food.display();
-}
-
-function moveAndDisplay(){
-  snake.move(delta());
-  hitFood();
-  checkEndGame();
-  display();
-}
-
-function bindKeys() {
-  $('body').keydown(function(e){
-    var arrowKeys = { 37 : 'l',
-                      38 : 'u',
-                      39 : 'r',
-                      40 : 'd'};
-    newDirection = arrowKeys[e.keyCode];
-    if (oppositeDirection(newDirection) && snake.hasBody()) {
-      endGame();
-    } else {
-      direction = newDirection;
+  this.oppositeDirection = function(newDirection) {
+    var opposites = {
+      'r': 'l',
+      'l': 'r',
+      'u': 'd',
+      'd': 'u'
     }
-  });
+    return opposites[direction] === newDirection
+  };
+
+  this.delta = function() {
+    return this.changePosition(direction);
+  };
+
+  this.hitEdge = function() {
+    return (snake.head()[0] > size || // hit right
+            snake.head()[0] < 0    || // hit left
+            snake.head()[1] > size || // hit bottom
+            snake.head()[1] < 0)      // hit top
+  };
+
+  this.checkEndGame = function() {
+    if (snake.hitSelf() || this.hitEdge()) {
+      this.endGame();
+    }
+  };
+
+  this.endGame = function() {
+    clearInterval(timer);
+    this.displayFinal();
+  };
+
+  this.displayFinal = function() {
+    console.log('You lose :(');
+  };
+
+  this.hitFood = function() {
+    if(snake.hitCell(food.getLocation())) {
+      snake.lengthen(this.delta());
+      food.generate(size);
+      return true;
+    }
+    return false;
+  };
+
+  return this.init();
 }
 
-function init() {
-  board = new Board('#board tbody', size);
-  snake = new Snake(size);
-  food = new Food(size);
-  board.display();
-  display();
-  bindKeys();
-  timer = setInterval(moveAndDisplay, 500);
-}
 
 $(function() {
-  init();
+  game = new Game();
 });
