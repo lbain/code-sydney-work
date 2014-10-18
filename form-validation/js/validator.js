7ad9f91821ed8d45ad1e52c95c2966ffd92d0734
Lucy Bain <lucybain@gmail.com>
2014-10-18 21:36:34 +1100

    Improvements to the form validator

--- form-validation/js/validator.js
+++ form-validation/js/validator.js
@@ -3,43 +3,62 @@ var Validator = {
   passwordRegex: /^[a-z]+$/,
   numberRegex: /^[0-9]+$/,
   countryRegex:/[a-zA-Z]{2,}/,
+  confirmation: ['email', 'password'],
 
-  valid: function(validateMethod, given){
-    return this[validateMethod+'Regex'].exec(given);
+  validateRegexes: function(validateMethod, given){
+    return this[validateMethod+'Regex'].test(given);
   },
-
-  email_error_message: 'You need to provide a valid email address',
-  password_error_message: 'Passwords must be all lowercase letters',
-  number_error_message: 'Must be a number',
-  country_error_message: 'Must be a country name',
-
-  validate_field: function($el){
-    var validateMethod = $el.data('validate-as') || $el.attr('type');
-    console.log(validateMethod);
-    var $errorMessage = $el.closest('.form-input').find('.error-msg');
-    if(!this.valid(validateMethod, $el.val())) {
-      $errorMessage.text(this[validateMethod + '_error_message']);
-      return false;
-    } else {
-      $errorMessage.text('');
+  validateConfirmations: function($el) {
+    var name = $el.attr('name')
+    if(name.indexOf('confirmation') == -1) {
       return true;
     }
+    var type = $el.attr('type');
+    var confirmations = $('input[type=' + type + ']');
+    return $(confirmations[0]).val() == $(confirmations[1]).val();
+  },
+  valid: function(validateMethod, $el){
+    return this.validateRegexes(validateMethod, $el.val()) && this.validateConfirmations($el);
+  },
+  validateField: function($el){
+    var validateMethod = this.validateMethod($el);
+    var valid = this.valid(validateMethod, $el);
+    this.validateConfirmations($el);
+    this.displayErrorMessage($el, valid, validateMethod);
+    return valid;
+  },
+  validateMethod: function($el) {
+    return $el.data('validate-as') || $el.attr('type');
+  },
+  displayErrorMessage: function($el, valid, validateMethod) {
+    $el.closest('.form-input').toggleClass('error', !valid);
+  },
+  numInvalid: function($submit) {
+    var inputs = $submit.closest('form').find('input').not(':input[type=submit]');
+    return $.grep(inputs, function( input, index ) {
+      return !Validator.validateField($(input));
+    }).length;
+  },
+  isValidForm: function($submit) {
+    var valid = this.numInvalid($submit) == 0;
+    $submit.toggleClass('valid', valid);
+    return valid;
   }
 };
 
 $(function() {
+
   $('form').focusout(function(event){
     var $target = $(event.target)
     if($target.is('input') && $target.val()){
-      Validator.validate_field($target);
+      Validator.validateField($target);
     }
   });
+
   $('form').submit(function(event){
-    var inputs = $(event.target).closest('form').find('input').not(':input[type=submit]');
-    var numInvalid = $.grep(inputs, function( input, index ) {
-      return !Validator.validate_field($(input));
-    }).length;
-    if(numInvalid > 0){
+    if(Validator.isValidForm($('input[type=submit]'))){
+      alert('succes!');
+    } else {
       event.preventDefault();
     }
   });
